de5580a7041a0a3f67da5f2159e9e63694e2769e
hayoon <gkdbs09@naver.com>
2022-04-18 02:27:34 +0900

    Time-Conversion

---
+++ Time_Conversion.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+
+    hour = s[0:2]
+    rest = s[2:8]
+
+    #AM
+    if (s[8:] =="AM"):
+        return s[0:-2]
+    #PM
+    else:
+        if (hour == "12"):
+            hour = "00"
+        else:
+          hour = str(int(hour)+12)
+
+        return hour+rest        
+
+s = "06:40:03AM"
+print(timeConversion(s))
