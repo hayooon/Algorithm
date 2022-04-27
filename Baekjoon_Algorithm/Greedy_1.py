1fc9400e1222bd6ef1d1da0314c1cbefff55566b
hayoon <gkdbs09@naver.com>
2022-04-27 14:29:22 +0900

    BJ_Greedy2_ATM

--- Baekjoon_Algorithm/Greedy_1.py
+++ Baekjoon_Algorithm/Greedy_1.py
@@ -2,24 +2,7 @@
 
 sugar = int(input())
 
-count = 0
-
-# while(True):
-#     if sugar < 3:
-#         print(-1)
-#         break
-#     else:
-#         sugar -= 3
-#         count += 1
-
-#     if sugar % 5 == 0:
-#         count += sugar // 5
-#         sugar %= 5
-    
-#     if sugar == 0:
-#         print (count)
-#         break    
-
+count = 0 
 
 while(True):
     if sugar < 3:
