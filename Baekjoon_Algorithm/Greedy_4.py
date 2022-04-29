71d693897ad60ae358a42a868585af9ea93d8889
hayoon <gkdbs09@naver.com>
2022-04-29 23:56:38 +0900

    BJ_Greedy4_회의실 배정

---
+++ Baekjoon_Algorithm/Greedy_4.py
@@ -0,0 +1,31 @@
+# < 회의실 배정 >
+
+import re
+from turtle import st
+
+
+n = int(input())
+meeting = []
+for i in range (n):
+    meeting.append(list(map(int,input().split())))
+
+meeting.sort(key = lambda x: x[0])
+meeting.sort(key = lambda x: x[1])
+
+print(meeting)
+
+count = 0
+finish = 0
+for meet in meeting:
+    if meet[0] >= finish:
+        count += 1
+        finish = meet[1]
+    else: 
+        continue
+
+print(count)   
+
+    
+
+
+
