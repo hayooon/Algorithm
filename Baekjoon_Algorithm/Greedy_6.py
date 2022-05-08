a215546405866eeee534152e4abe08fd5d41e955
hayoon <gkdbs09@naver.com>
2022-05-09 02:53:29 +0900

    BJ_Greed6_잃어버린 괄호

--- Baekjoon_Algorithm/Greedy_6.py
+++ Baekjoon_Algorithm/Greedy_6.py
@@ -1,40 +1,30 @@
-# < 잃어버린 괄호 >
+# # < 잃어버린 괄호 >
 
-# eq = input()
-# if '+' not in eq or '-' not in eq:
-#     result = eval(''.join(eq))
-# else:
-#     eq = eq.split('-')
-#     result = 0
-
-#     for i in range(len(eq)):
-#         if i == 0:
-#             result += int(eq[i])
-#         else:
-#             result = result - eval(eq[i])   
-
-# print(result)
 
 eq = input()
 result = 0
+
 if '-' not in eq:
     for i in eq.split('+'):
         result += int(i)
+
 elif '+' not in eq:
-    for i in range(len(eq.split('-'))):
+    eq = eq.split('-')
+    for i in range(len(eq)):
         if i == 0:
-            result += int(i)
-        else:
-            result -= int(i)
-else:
-    eq = input().split('-')
-    for i in range (len(eq)):
-        if i == 0 :
             result += int(eq[i])
         else:
-            small = eq[i].split('+')
-            sum = 0
-            for j in range (len(small)):
-                sum += int(small[j])
-            result -= sum
+            result -= int(eq[i])
+            
+# 섞여있는 경우            
+else:
+    eq = eq.split('-')
+    for i in range (len(eq)):
+        small = eq[i].split('+')
+        sum = 0
+        for j in range (len(small)):
+            sum += int(small[j])
+        if i == 0:
+            result += sum   
+        else: result -= sum
 print(result)
