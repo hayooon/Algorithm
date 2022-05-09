670c86d93ebb54912e74ab0808fd8a6d303551c0
hayoon <gkdbs09@naver.com>
2022-05-10 01:55:36 +0900

    BJ_Greedy9_30

---
+++ Baekjoon_Algorithm/Greedy_9.py
@@ -0,0 +1,12 @@
+# 30
+
+import re
+letter = list(map(int,input()))
+if 0 not in letter or sum(letter) % 3 != 0:
+    print(-1)
+else:
+    letter.sort(reverse=True)
+    print("".join(map(str,letter)))
+
+
+
